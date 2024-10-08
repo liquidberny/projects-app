c257c6ace875c5d4af50d5fb9b9be22aa6dafeec
liquidberny <bernardo.urquijo.bu@gmail.com>
2024-10-08 08:31:46 -0700

    redireccionar si no existe el proyecto

--- src/modules/projects/views/ProjectView.vue
+++ src/modules/projects/views/ProjectView.vue
@@ -1,5 +1,36 @@
 <template>
-  <bread-crumbs :name="project?.name ?? 'No name'"></bread-crumbs>
+  <div class="w-full">
+    <section class="m-2">
+      <bread-crumbs :name="project?.name ?? 'No name'"></bread-crumbs>
+    </section>
+    <section>
+      <div class="overflow-x-auto">
+        <table class="table">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th class="w-14">Completada</th>
+              <th>Tarea</th>
+              <th>Completada en</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="hover">
+              <th></th>
+              <td>
+                <input
+                  type="text"
+                  class="input input-primary w-full opacity-60 transition-all hover:opacity-100"
+                  placeholder="Nueva tarea"
+                />
+              </td>
+              <td>Desktop Support Technician</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -7,11 +38,13 @@ import BreadCrumbs from '@/modules/common/components/BreadCrumbs.vue';
 import { useProjectStore } from '../store/projects.store';
 import { ref, watch } from 'vue';
 import type { Project } from '../interfaces/project.interface';
+import { useRouter } from 'vue-router';
 
 interface Props {
   id: string;
 }
 
+const router = useRouter();
 const props = defineProps<Props>();
 const projectStore = useProjectStore();
 const project = ref<Project | undefined>();
@@ -22,6 +55,9 @@ watch(
   () => props.id,
   () => {
     project.value = projectStore.projectList.find((project) => project.id === props.id);
+    if (!project.value) {
+      router.replace('/');
+    }
   },
   {
     inmediate: true,
