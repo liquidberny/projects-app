ff4d03729499e64ce4380fe9baa9dc5a6bb714a1
liquidberny <bernardo.urquijo.bu@gmail.com>
2024-10-11 09:36:13 -0700

    pruebas en FabButton

---
+++ tests/modules/common/components/FabButton.test.ts
@@ -0,0 +1,25 @@
+import { shallowMount } from '@vue/test-utils';
+import FabButton from '@/modules/common/components/FabButton.vue';
+
+describe('<FabButton />', () => {
+  test('renders with default position', () => {
+    const wrapper = shallowMount(FabButton);
+
+    expect(wrapper.props().position).toBe('bottom-right');
+    const buttonClasses = wrapper.find('button').classes();
+    const classesToHave = ['btn', 'btn-circle', 'btn-secondary', 'fixed', 'bottom-right'];
+
+    expect(buttonClasses).toEqual(classesToHave);
+  });
+
+  test('renders with top-left possition', () => {
+    const wrapper = shallowMount(FabButton, {
+      props: {
+        position: 'top-left',
+      },
+    });
+
+    const button = wrapper.find('button');
+    expect(button.classes()).toContain('top-left');
+  });
+});
