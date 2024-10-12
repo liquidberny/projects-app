315d6f2cf8fa808c6dec016f49048a1fdb1ce123
liquidberny <bernardo.urquijo.bu@gmail.com>
2024-10-11 17:27:05 -0700

    test projectView

---
+++ tests/modules/projects/views/ProjectsView.test.ts
@@ -0,0 +1,42 @@
+import { useProjectStore } from '@/modules/projects/store/projects.store';
+import ProjectsView from '@/modules/projects/views/ProjectsView.vue';
+import { createTestingPinia } from '@pinia/testing';
+import { mount } from '@vue/test-utils';
+import { fakeProjects } from '../../../mocks/projects.fake';
+
+describe('<ProjectsView />', () => {
+  const wrapper = mount(ProjectsView, {
+    global: {
+      plugins: [createTestingPinia],
+    },
+  });
+
+  const store = useProjectStore();
+
+  beforeEach(() => {
+    store.$patch({
+      projects: fakeProjects,
+    });
+  });
+
+  test('should render with projects', () => {
+    const tableRows = wrapper.findAll('tbody tr');
+    expect(tableRows.length).toBe(3);
+
+    tableRows.forEach((row, index) => {
+      const project = fakeProjects[index];
+      const cell = row.findAll('td');
+      expect(cell.at(0)?.text()).toBe(project.name);
+      expect(cell.at(1)?.text()).toBe(project.tasks.length.toString());
+    });
+  });
+
+  test('should call addProject method on modal', () => {
+    const inputModal = wrapper.findComponent({ name: 'InputModal' });
+    const newProjectName = 'New Project';
+    inputModal.vm.$emit('value', newProjectName);
+
+    expect(store.addProject).toHaveBeenCalled();
+    expect(store.addProject).toHaveBeenCalledWith(newProjectName);
+  });
+});
